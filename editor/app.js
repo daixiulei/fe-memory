f80a79335bf2f02bdb8c63499380a597678f286e
代秀磊 <daixiulei@outlook.com>
2022-06-24 16:56:04 +0800

    feat: leetcode

--- editor/app.js
+++ editor/app.js
@@ -1,8 +1,11 @@
-var app = require('koa')()
-  , logger = require('koa-logger')
-  , json = require('koa-json')
-  , views = require('koa-views')
-  , onerror = require('koa-onerror');
+var Koa = require('koa'),
+  logger = require('koa-logger'),
+  json = require('koa-json'),
+  views = require('koa-views'),
+  onerror = require('koa-onerror'),
+  mount = require('koa-mount');
+
+const app = new Koa();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,30 +14,32 @@ var users = require('./routes/users');
 onerror(app);
 
 // global middlewares
-app.use(views('views', {
-  root: __dirname + '/views',
-  default: 'jade'
-}));
+app.use(
+  views('views', {
+    root: __dirname + '/views',
+    default: 'jade',
+  })
+);
 app.use(require('koa-bodyparser')());
 app.use(json());
 app.use(logger());
 
-app.use(function *(next){
-  var start = new Date;
+app.use(function* (next) {
+  var start = new Date();
   yield next;
-  var ms = new Date - start;
+  var ms = new Date() - start;
   console.log('%s %s - %s', this.method, this.url, ms);
 });
 
 app.use(require('koa-static')(__dirname + '/public'));
 
 // routes definition
-app.use(index.routes(), index.allowedMethods());
-app.use(users.routes(), users.allowedMethods());
+app.use(mount('/', index.middleware()));
+app.use(mount('/users', users.middleware()));
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
 module.exports = app;
--- editor/routes/users.js
+++ editor/routes/users.js
@@ -2,11 +2,11 @@ var router = require('koa-router')();
 
 router.prefix('/users');
 
-router.get('/', function *(next) {
+router.get('/', function* (next) {
   this.body = 'this is a users response!';
 });
 
-router.get('/bar', function *(next) {
+router.get('/bar', function* (next) {
   this.body = 'this is a users/bar response!';
 });
 
